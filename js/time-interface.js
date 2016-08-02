e9bf566a76ce23b5d58ccf316fcf347e74420118
Georgina Van Dort and Julia Dickey <georgina+julia@>
2016-08-02 16:49:02 -0700

    Make everything awesome...with gongs

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   $('#time').text(moment());
 
+
+function update() {
+  $('#time').text(moment().format('D. MMMM YYYY H:mm:ss'));
+}
+setInterval(update, 1000);
+
   var alarms = [];
 
   $("#setbutton").click(function(event){
@@ -10,13 +16,14 @@ $(document).ready(function(){
     var mins = $("#mins").val();
     var time = hour + ":" + mins + " " + ampm;
 
-    alarms.push(time)
+    alarms.push(time);
     console.log(alarms);
     $("#showalarm").append("<li>" + "Alarm set for " + time + "</li>");
     alarms.forEach(function(alarm){
         if (time === moment().format('LT')) {
-        console.log("ALARM!!");
+        var audio = new Audio('sound.mp3');
         $("#showalarm").text("Wake Up!");
+        audio.play();
       }
     });
   });
