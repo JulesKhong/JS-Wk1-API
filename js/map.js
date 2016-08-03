c6b71ac10c3ac74a873d7457a7808619e14afb7a
Colin W Kingen and Julia Dickey <colin@>
2016-08-03 16:44:22 -0700

    get google map to appear on page

--- js/map.js
+++ js/map.js
@@ -1,7 +1,22 @@
-var apiKey = require('./../.env').mapApiKey;
+var apiKey = "AIzaSyAGZqHCSiz3soGvUCJCZWtJ3gMC50fMuoU";
 
 function Map() {
 
 }
+Map.prototype.initMap = function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+      center: {lat: 44.540, lng: -78.546},
+      zoom: 8
+  });
+}
+
+
+
+// Map.prototype.show = function() {
+//   $.get('https://maps.googleapis.com/maps/api/js?key=AIzaSyAGZqHCSiz3soGvUCJCZWtJ3gMC50fMuoU&callback=initMap').then(function(response) {
+//   })
+// }
+
 
 exports.mapModule = Map;
--- js/search-interface.js
+++ js/search-interface.js
@@ -7,7 +7,7 @@ var displayResults = function(manufacturer, results) {
   results.bikes.forEach(function(bike){
     if (bike.thumb){
       $('#showResults').append("<div class='col-sm-3 each_bike'><img src=" + bike.thumb + "><div class='li-div'><li>" + bike.title +"</li></div></div>");
-    };
+    }
   });
 };
 
@@ -17,17 +17,20 @@ var displayStolen = function(zip, radius, results) {
   results.bikes.forEach(function(bike) {
     if (bike.thumb){
       $('#showResults').append("<div class='col-sm-3 each_bike'><img src=" + bike.thumb + "><div class='li-div'><li>" + bike.title +"</li></div></div>");
-    };
+    }
   });
 };
 
 $(document).ready( function() {
+
+  var newMap = new Map();
+  newMap.initMap();
   $('#search-man').submit( function(event) {
     event.preventDefault();
     newSearch = new Search();
     var manu = $('#manufacturer').val();
     newSearch.searchManufacturer(manu, displayResults);
-
+    $('#map').append(newMap);
   });
   $('#search_stolen').submit( function(event) {
     event.preventDefault();
